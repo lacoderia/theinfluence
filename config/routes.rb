0605048dc774d07d87d64e40a150bc7c6b1ffea4
owen8505 <luis.sanchez.franco@gmail.com>
2014-06-05 14:29:15 -0500

    Se agregó resources :users en display/index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'display#index'
+  root :to => "display#index"
   get "nosotros" => "home#nosotros"
   get "agencias" => "home#agencias"
   get "anunciantes" => "home#anunciantes"
@@ -12,5 +8,13 @@ Rails.application.routes.draw do
   get "targeted_video" => "home#targeted_video"
   get "media" => "home#media"
   get "contacto" => "home#contacto"
+
+
+
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :users
+
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+  resources "contacts", only: [:new, :create]
 
 end
