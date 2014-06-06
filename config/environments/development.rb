d1bf73482ae3cc4356488c1fa4b98280159e7ced
owen8505 <luis.sanchez.franco@gmail.com>
2014-06-05 19:32:11 -0500

    Se añadió la ruta de las fuentes en development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.enabled = true
+  config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
