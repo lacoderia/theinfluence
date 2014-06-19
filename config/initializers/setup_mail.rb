818f339d8687b266721198a7e30f350a425921a3
Benjamin Hernandez <benjamin@coderia.mx>
2014-06-19 12:29:58 -0500

    Agregado nuevo correo

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "tonklis@gmail.com",
-  :password             => "Oportunidad2013",
+  :domain               => "theinfluence.co",
+  :user_name            => "ventas@theinfluence.co",
+  :password             => "Chamba123",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
