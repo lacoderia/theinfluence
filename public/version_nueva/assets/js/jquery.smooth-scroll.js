393a09baa9233b0477b41db2248bb0eb02bf8015
Benjamin Hernandez <benjamin@coderia.mx>
2014-06-27 17:16:07 -0500

    Se quitó la versión dummy.

--- public/version_nueva/assets/js/jquery.smooth-scroll.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(e){e(".scroll").click(function(t){t.preventDefault();e("html,body").animate({scrollTop:e(this.hash).offset().top},1e3)})});
--- public/version_nueva/assets/js/scriptisa.js
+++
@@ -1,4 +0,0 @@
-$('#imgDetail li img').hover(function(){
-        $('#bigImg').show();        
-        $('#bigImg').attr('src',$(this).attr('src'));
-});
