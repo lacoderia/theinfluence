b9b6b223d4f16d300a2190f3cc5eb8504aa29a7c
Benjamin Hernandez <benjamin@coderia.mx>
2014-07-07 20:02:58 -0500

    Se agregó loader.

--- app/assets/javascripts/product_detail.js
+++ app/assets/javascripts/product_detail.js
@@ -1,17 +1,20 @@
+
 function enviar_compra(user_id, combo_id, addon_combo_id){
   if (addon_combo_id != null){
     var data = {user_id: user_id, combo_id: combo_id, addon_combo_id: addon_combo_id};
   }else{
     var data = {user_id: user_id, combo_id: combo_id};
   }
-  //loader_on
+
+  $('html').loader('show');
   $.ajax({
     url: "/send_compra_email",
     data: data,
     success: function(){
-      //loader off
+      $('html').loader('hide');
       $('#mensaje_enviado_modal').modal('show');
     },error: function(){
+      $('html').loader('hide');
       alert("No se pudo enviar el correo, por favor intenta más tarde.");
     }
   });
@@ -20,14 +23,15 @@ function enviar_compra(user_id, combo_id, addon_combo_id){
 function enviar_cotizacion(user_id, product_id){
   var data = {user_id: user_id, product_id: product_id};
   
-  //loader_on
+  $('html').loader('show');
   $.ajax({
     url: "/send_cotiza_email",
     data: data,
     success: function(){
-      //loader off
+      $('html').loader('hide');
       $('#mensaje_enviado_modal').modal('show');
     },error: function(){
+      $('html').loader('hide');
       alert("No se pudo enviar el correo, por favor intenta más tarde.");
     }
   });
@@ -36,14 +40,15 @@ function enviar_cotizacion(user_id, product_id){
 function cotizar_addons(user_id, addon_id, product_id){
   var data = {user_id: user_id, addon_id: addon_id, quantity: $("#addon_"+addon_id).val(), product_id: product_id};
 
-  //loader_on
+  $('html').loader('show');
   $.ajax({
     url: "/send_addons_cotiza_email",
     data: data,
     success: function(){
-      //loader off
+      $('html').loader('hide');
       $('#mensaje_enviado_modal').modal('show');
     },error: function(){
+      $('html').loader('hide');
       alert("No se pudo enviar el correo, por favor intenta más tarde.");
     }
   });
