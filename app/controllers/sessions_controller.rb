4fea868c824a1e4469ac66716e6aedc5a47e65bb
Benjamin Hernandez <benjamin@coderia.mx>
2014-07-14 19:25:50 -0500

    Correcciones #14715367193106, #14715367193111, #14715367193113, #14715367193115

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < Devise::SessionsController
   def after_sign_in_path_for(user)
      if not user.active?
        sign_out user
-       flash[:alert] = "Cuenta inactiva."
+       flash[:alert] = "Tu cuenta ha sido desactivada. No te preocupes, esto puede ser por falta de pago o por alguna otra razón. Contacta a tu representante para volver a activarla."
        flash[:notice] = nil
        return :root
      else
